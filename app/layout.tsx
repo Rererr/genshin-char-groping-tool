05175e2be06ad22c5656c4e4454510a257303716
Ren Watanabe <ren.watanabe.go@gmail.com>
2024-05-29 14:32:59 +0900

    tag managerテスト

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import React from "react";
+import {GoogleAnalytics, GoogleTagManager} from "@next/third-parties/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Genshin-Character-Grouping-Tool",
-  description: "",
+    title: "Genshin-Character-Grouping-Tool",
+    description: "",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    const gtmId = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || '';
+    return (
+        <html lang="ja">
+        <body className={inter.className}>{children}</body>
+        <GoogleTagManager gtmId={gtmId}/>
+        <GoogleAnalytics gaId={gtmId}/>
+        </html>
+    );
 }
